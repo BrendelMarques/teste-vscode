da3b9dd28ae0b6a1c6361213cc928f4d84147085
BrendelMarques <brendel.marques@fatec.sp.gov.br>
2022-10-02 17:45:36 -0300

    Second commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("Hello World")
+nome = input("Informe seu nome: ")
+sobrenome = input("Informe seu sobrenome: ")
+
+nomecompleto = nome + (" ") + sobrenome
+
+print(f"Seu nome completo é {nomecompleto}")
