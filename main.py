6915aa32c9af0c02acd99c8ba2b33ec41918ac33
BrendelMarques <brendel.marques@fatec.sp.gov.br>
2022-10-02 17:52:34 -0300

    Third commit

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 print("Hello World")
 nome = input("Informe seu nome: ")
 sobrenome = input("Informe seu sobrenome: ")
+print()
 
 nomecompleto = nome + (" ") + sobrenome
 
 print(f"Seu nome completo é {nomecompleto}")
+print()
+
+idade = int(input('Informe a sua idade: '))
+print()
+dias = idade * 365
+print(f'Você tem {dias} dias de vida!')
